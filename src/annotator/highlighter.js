067deb461c4bd88bd8ead1ed3ebea85e1c42b043
smjaques <sjaques@calpoly.edu>
2021-03-09 09:32:06 -0800

    fixing bracket it didnt like

--- src/annotator/highlighter.js
+++ src/annotator/highlighter.js
@@ -267,8 +267,10 @@ export function highlightRange(
     highlightEl.className = cssClass;
     //here is where we can call another function to get the color for the user
     //color-->index number in the 'user-color' class
-    if (colorNum != -1)
+    if (colorNum != -1) {
       highlightEl.classList.toggle('user-color' + Math.round(colorNum), true);
+    }
+      
 
     nodes[0].parentNode.replaceChild(highlightEl, nodes[0]);
     nodes.forEach(node => highlightEl.appendChild(node));
