a121e38880e6d4689828d46087f2b16c9d407dd6
wfuchs <wfuchs@calpoly.edu>
2021-05-08 13:03:15 -0700

    refactor into one controller

---
+++ src/doodle/displayCanvas.js
@@ -0,0 +1,40 @@
+import { createElement } from 'preact';
+import { Canvas } from './canvas';
+import propTypes from 'prop-types';
+
+const DisplayCanvas = ({ container, lines }) => {
+  const boundingRect = container.getBoundingClientRect();
+  return (
+    <div
+      style={{
+        position: 'absolute',
+        top: boundingRect.top + window.scrollY,
+        left: boundingRect.left + window.scrollX,
+        zIndex: 9999,
+        pointerEvents: 'none',
+      }}
+    >
+      <Canvas
+        width={boundingRect.width}
+        height={
+          Math.min(
+            boundingRect.height,
+            10000
+          ) /*Canvas starts to lag over 10k, doesnt work over 32k*/
+        }
+        handleMouseDown={() => {}}
+        handleMouseUp={() => {}}
+        handleMouseLeave={() => {}}
+        handleMouseMove={() => {}}
+        lines={lines}
+      />
+    </div>
+  );
+};
+
+DisplayCanvas.propTypes = {
+  lines: propTypes.array.isRequired,
+  container: propTypes.object.isRequired,
+};
+
+export { DisplayCanvas };
